8a88b0b42eedf1768e1aa3f19095fa27cbcb40a6
Joshua Palermo <josh@Joshuas-MacBook-Pro.local>
2013-11-07 19:37:00 -0500

    Initial Commit

---
+++ GameTrader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameTrader
+//
+//  Created by Joshua Palermo on 11/7/13.
+//  Copyright (c) 2013 Joshua Palermo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GameTrader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GameTrader
+//
+//  Created by Joshua Palermo on 11/7/13.
+//  Copyright (c) 2013 Joshua Palermo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
